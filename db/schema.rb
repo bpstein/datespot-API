4dc89a76922e27caf0c72d34a86095bfccf4def7
Ralin Chimev <ralin.chimev@gmail.com>
2017-06-07 17:08:07 +0300

    Re-create database schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,103 +13,103 @@
 ActiveRecord::Schema.define(version: 20170404175244) do
 
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.string   "author_type"
-    t.integer  "author_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_id", null: false
+    t.string "resource_type", null: false
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
   create_table "admin_users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.text     "content"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_categories_on_email", unique: true
     t.index ["reset_password_token"], name: "index_categories_on_reset_password_token", unique: true
   end
 
   create_table "datespots", force: :cascade do |t|
-    t.string   "name"
-    t.text     "short_description"
-    t.text     "long_description"
-    t.string   "location"
-    t.string   "website"
-    t.integer  "price_range"
-    t.date     "start_date"
-    t.date     "end_date"
-    t.boolean  "featured",          default: false, null: false
-    t.integer  "category_id"
-    t.time     "mon_open"
-    t.time     "mon_close"
-    t.time     "tue_open"
-    t.time     "tue_close"
-    t.time     "wed_open"
-    t.time     "wed_close"
-    t.time     "thu_open"
-    t.time     "thu_close"
-    t.time     "fri_open"
-    t.time     "fri_close"
-    t.time     "sat_open"
-    t.time     "sat_close"
-    t.time     "sun_open"
-    t.time     "sun_close"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.string "name"
+    t.text "short_description"
+    t.text "long_description"
+    t.string "location"
+    t.string "website"
+    t.integer "price_range"
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "featured", default: false, null: false
+    t.integer "category_id"
+    t.time "mon_open"
+    t.time "mon_close"
+    t.time "tue_open"
+    t.time "tue_close"
+    t.time "wed_open"
+    t.time "wed_close"
+    t.time "thu_open"
+    t.time "thu_close"
+    t.time "fri_open"
+    t.time "fri_close"
+    t.time "sat_open"
+    t.time "sat_close"
+    t.time "sun_open"
+    t.time "sun_close"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_datespots_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
