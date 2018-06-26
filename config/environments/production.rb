b7bd979406ecc211286eaffa75f4642864d602d6
anup <anup1710@gmail.com>
2018-06-26 23:26:32 +0530

    Remove asset pipeline vulnerability in PROD

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
