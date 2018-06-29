d5fb2db666ac9f14316bfebc859fecc973bf9d61
Ben Stein <bumstain@gmail.com>
2018-06-30 01:14:13 +0900

    Updated Gemfile dependencies to resolve security vulnerabilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180414110611) do
+ActiveRecord::Schema.define(version: 2018_04_14_110611) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
